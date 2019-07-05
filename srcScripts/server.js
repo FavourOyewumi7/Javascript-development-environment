3d802eeaf51d7d62f32caeb8507005d141ac2031
FavourOyewumi7 <oyewumifavour65@gmail.com>
2019-07-05 17:52:34 +0100

    created the server with express

--- srcScripts/server.js
+++ srcScripts/server.js
@@ -2,11 +2,11 @@ var express = require('express');
 var path = require('path');
 var open = require('open');
 
-var port = 8080;
+var port = 8000;
 
 var app = express();
 
-app.get('/', function(res,req){
+app.get('/', function(req,res){
     res.sendFile(path.join(__dirname, '../src/index.html'));
 })
 
